d16a5b8d678db402fa5b38e8aa2061f9d225f0fd
Sunil Gunisetty <sunilgunisetty@gmail.com>
2021-02-17 16:32:22 -0500

    add kitty

--- home.nix
+++ home.nix
@@ -138,6 +138,48 @@
     enableZshIntegration = true;
   };
 
+  programs.kitty = {
+    enable = true;
+    settings = {
+      font_size = "13";
+      disable_ligature = "never";
+      cursor_shape = "beam";
+      tab_bar_style = "separator";
+      macos_titlebar_color = "system";
+      macos_option_as_alt = "yes";
+    };
+    font = {
+      package = pkgs.fira-code;
+      name = "Fira Code Retina";
+    };
+    extraConfig = ''
+      # Theme from https://raw.githubusercontent.com/dexpota/kitty-themes/master/themes/Monokai_Soda.conf
+      background           #191919
+      foreground           #c4c4b5
+      #cursor               #f6f6ec
+      # Green cursor color
+      cursor               #15b61a
+      selection_background #343434
+      color0               #191919
+      color8               #615e4b
+      color1               #f3005f
+      color9               #f3005f
+      color2               #97e023
+      color10              #97e023
+      color3               #fa8419
+      color11              #dfd561
+      color4               #9c64fe
+      color12              #9c64fe
+      color5               #f3005f
+      color13              #f3005f
+      color6               #57d1ea
+      color14              #57d1ea
+      color7               #c4c4b5
+      color15              #f6f6ee
+      selection_foreground #191919
+    '';
+  };
+
   programs.git = {
     enable = true;
     userEmail = "sunilgunisetty@gmail.com";
