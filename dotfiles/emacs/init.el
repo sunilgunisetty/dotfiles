a3d8c929c5f06d560d0d338f171844497bc75fb2
Sunil Gunisetty <sunilgunisetty@gmail.com>
2021-06-22 11:12:58 -0400

    update ivy, counsel, flex

--- dotfiles/emacs/init.el
+++ dotfiles/emacs/init.el
@@ -188,36 +188,64 @@
 ;; ########################################################################
 
 (use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-f" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
   :init
-  (setq ivy-initial-inputs-alist nil)
-  :bind (("C-x b" . ivy-switch-buffer))
-  :custom
-  (progn (setq ivy-use-virtual-buffers t)
-         (setq ivy-display-style 'fancy)
-         (setq ivy-count-format "%d/%d "))
-  :config
   (ivy-mode 1)
-  (use-package ivy-hydra
-    :defer t)
-  (use-package flx))
+  :config
+  (setq ivy-use-virtual-buffers t)
+  (setq ivy-wrap t)
+  (setq ivy-count-format "(%d/%d) ")
+  (setq enable-recursive-minibuffers t)
+
+  ;; Use different regex strategies per completion command
+  (push '(completion-at-point . ivy--regex-fuzzy) ivy-re-builders-alist) ;; This doesn't seem to work...
+  (push '(swiper . ivy--regex-ignore-order) ivy-re-builders-alist)
+  (push '(counsel-M-x . ivy--regex-ignore-order) ivy-re-builders-alist)
+
+  ;; Set minibuffer height for different commands
+  (setf (alist-get 'counsel-projectile-ag ivy-height-alist) 15)
+  (setf (alist-get 'counsel-projectile-rg ivy-height-alist) 15)
+  (setf (alist-get 'swiper ivy-height-alist) 15)
+  (setf (alist-get 'counsel-switch-buffer ivy-height-alist) 7))
+
+(use-package ivy-hydra
+  :defer t
+  :after hydra)
+
+(use-package flx  ;; Improves sorting for fuzzy-matched results
+  :after ivy
+  :defer t
+  :init
+  (setq ivy-flx-limit 10000))
 
 (use-package all-the-icons-ivy
   :init (all-the-icons-ivy-setup)
   :ensure t)
 
-(use-package swiper
-  :bind (("C-s" . swiper)
-         ("C-r" . swiper)
-         ("C-c C-r" . ivy-resume)))
-
 (use-package counsel
-  :after swiper
-  :config
-  (counsel-mode)
+  :demand t
   :bind
-  (("M-y" . counsel-yank-pop)
-   :map ivy-minibuffer-map
-   ("M-y" . ivy-next-line)))
+  (("M-x" . counsel-M-x)
+   ("C-x b" . counsel-ibuffer)
+   ("C-x C-b" . counsel-switch-buffer)
+   ("C-x C-f" . counsel-find-file)
+   ("M-y" . counsel-yank-pop))
+  :config
+  (setq ivy-initial-inputs-alist nil))
 
 (use-package projectile
   :diminish projectile-mode
